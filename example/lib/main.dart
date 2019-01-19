d583359a7491a0177103bf50dba206ed2aac7baa
Kshitij Gupta <kshitijgm@gmail.com>
2019-01-20 01:04:23 +0530

    diracplugin: Moar refactor
    Signed-off-by: Kshitij Gupta <kshitijgm@gmail.com>

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,6 +1,6 @@
 import 'dart:async';
 
-import 'package:dirac/dirac.dart';
+import 'package:diracplugin/diracplugin.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
